c6d56acfcd929a39fcf47d0ad3fc0a1566a45ca5
jifeixing <jifeixing@163.com>
2014-12-18 18:58:17 +0800

    M    trunk/www/js/controllers.js M    trunk/www/templates/coverage.html M    trunk/www/templates/products.html

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,6 +9,11 @@ angular.module('iagent.controllers', ['iagent.services'])
       this.action = {};
     }
   };
+  $scope.prods = {
+  	selectProduct: {},
+  	selectedCoverage: {}
+  };
+  
 })
 
 .controller('HomeCtrl', function($scope, $stateParams) {
@@ -70,103 +75,82 @@ angular.module('iagent.controllers', ['iagent.services'])
   }
 })
 
-.controller('ProductsCtrl', function($scope, $stateParams) {
-  $scope.groups = [];
-  $scope.selectProduct;
-  $scope.groups[0] = {
-  	name: '车险',
-  	code: '',
-    items: [],
-    show: false
-  };
-  $scope.groups[1] = {
-  	name: '寿险',
-  	code: '',
-    items: [],
-    show: false
-  };
-  $scope.groups[2] = {
-  	name: '旅行意外险',
-  	code: '',
-    items: [],
-    show: false
-  };
+.controller('ProductsCtrl', function($scope, $state) {
+  $scope.products = {groups:[]};
   
-  $scope.groups[0].items.push('车险详细介绍');
-  $scope.groups[1].items.push('寿险详细介绍');
-  $scope.groups[2].items.push('旅行意外险详细介绍');
-  // for (var i=0; i<4; i++) {
-    // $scope.groups[i] = {
-      // name: i,
-      // items: [],
-      // show: false
-    // };
-    // for (var j=0; j<3; j++) {
-      // $scope.groups[i].items.push(i + '-' + j);
-    // }
-  // }
-  
-  $scope.toggleGroup = function(group) {
-  	$scope.selectProduct = group;
-  	shrinkAllGroup();
-    group.show = !group.show;
-  };
-  
-  $scope.isGroupShown = function(group) {
-    return group.show;
-  };
-  
-  function shrinkAllGroup(){
-  	if($scope.groups){
-  		for (var i=0; i<$scope.groups.length; i++){
-  			$scope.groups[i].show = false;
-  		}
+  $scope.prodChanged = function (prod) {
+  	if(prod){
+  		$scope.prods.selectProduct = prod;
+  		$state.go('coverage');
   	}
-  }
+  };
+  
+  $scope.products.groups[0] = {
+  	name: '车险',
+  	code: 'vehicle',
+  	desc:'车险描述'
+  };
+  $scope.products.groups[1] = {
+  	name: '寿险',
+  	code: 'life',
+  	desc: '寿险描述'
+  };
+  $scope.products.groups[2] = {
+  	name: '旅行意外险',
+  	code: 'travel',
+  	desc: '旅行意外险描述'
+  };
 })
 
-.controller('CoverageCtrl', function($scope) {
-  $scope.groups = [];
-  $scope.groups[0] = {
-  	name: '机动车强制险',
-    items: [],
-    show: false
-  };
-  $scope.groups[1] = {
-  	name: '第三方责任险',
-    items: [],
-    show: false
-  };
-  $scope.groups[2] = {
-  	name: '玻璃险',
-    items: [],
-    show: false
-  };
-  $scope.groups[3] = {
-  	name: '盗抢险',
-    items: [],
-    show: false
-  };
-  $scope.groups[0].items.push('限额1000000');
-  $scope.groups[0].items.push('保额1000000');
-  $scope.groups[1].items.push('限额10000007');
-  $scope.groups[2].items.push('限额10000008');
-  $scope.groups[3].items.push('限额10000009');
-  // for (var i=0; i<4; i++) {
-    // $scope.groups[i] = {
-      // name: i,
-      // items: [],
-      // show: false
-    // };
-    // for (var j=0; j<3; j++) {
-      // $scope.groups[i].items.push(i + '-' + j);
-    // }
-  // }
+.controller('CoverageCtrl', function ($scope, $state) {
+  $scope.coverage = {groups:[]};
   
-  $scope.toggleGroup = function(group) {
-    group.show = !group.show;
+  $scope.selectChanged = function (group) {
+    if (group) {
+    	$scope.prods.selectedCoverage[group.hasSelected.code] = group.hasSelected;
+    }
   };
-  $scope.isGroupShown = function(group) {
-    return group.show;
+  
+  $scope.next = function () {
+  	var prodCover = $scope.prods;
   };
+  
+  if($scope.prods.selectProduct && $scope.prods.selectProduct.code === 'vehicle'){
+  	$scope.coverage.groups[0] = {
+	  	name: '机动车强制险',
+	  	code: 'OD_G',
+	  	desc: '机动车强制险描述',
+	    liability: [
+	    	{code:'OD_G',value:100000},
+	    	{code:'OD_G',value:20000}
+	    ]
+	  };
+	  $scope.coverage.groups[1] = {
+	  	name: '第三方责任险',
+	  	code: 'TPL_G',
+	  	desc: '第三方责任险描述',
+	  	liability: [
+	    	{code:'TPL_G',value:100000},
+	    	{code:'TPL_G',value:20000}
+	    ]
+	  };
+	  $scope.coverage.groups[2] = {
+	  	name: '自燃险',
+	  	code: 'FIRE_G',
+	  	desc: '自燃险描述',
+	  	liability: [
+	    	{code:'FIRE_G',value:100000},
+	    	{code:'FIRE_G',value:20000}
+	    ]
+	  };
+	  $scope.coverage.groups[3] = {
+	  	name: '盗抢险',
+	  	code: 'TH_G',
+	  	desc: '盗抢险描述',
+	  	liability: [
+	    	{code:'TH_G',value:100000},
+	    	{code:'TH_G',value:20000}
+	    ]
+	  };
+  }
 });
